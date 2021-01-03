68dda264dc590ffb8bad43fc8ace56437247bded
Edward Amor <edward.amor3@gmail.com>
2021-01-02 20:31:57 -0500

    feat: add totalsupply & transfer func to contract

---
+++ contracts/cloudtoken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.5;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+
+/// @title A contract for interacting with the clouds
+/// @author Edward Amor
+contract CloudToken {
+    using SafeMath for uint256;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    /// @dev Amount '_supply' is the total supply of tokens in existence
+    constructor(uint256 _supply) public {
+        _totalSupply = _supply;
+        _balances[msg.sender] = _supply;
+    }
+
+    /// @dev Return the total supply of tokens in existence
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /// @dev Transfer '_amount' of tokens to '_recipient' from sender
+    function transfer(address _recipient, uint256 _amount) external {
+        _balances[msg.sender] = _balances[msg.sender].sub(_amount);
+        _balances[_recipient] = _balances[_recipient].add(_amount);
+    }
+}
