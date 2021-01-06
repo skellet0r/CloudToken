f218004f240be0a02434c2e23f41118fa3acc4f5
Edward Amor <edward.amor3@gmail.com>
2021-01-05 23:59:26 -0500

    fix: modify usdToWei function to pass test
    Added the usage of the SafeCast library for better casting to uint256.

--- contracts/CloudToken.sol
+++ contracts/CloudToken.sol
@@ -3,15 +3,20 @@
 pragma solidity ^0.7.5;
 
 // Links to use when working on https://remix.ethereum.org/#optimize=true&runs=200&evmVersion=null&version=soljson-v0.7.5+commit.eb77ed08.js
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/SafeCast.sol";
 import "./Token.sol";
 
 
 /// @title A contract for interacting with the clouds
 /// @author Edward Amor
 contract CloudToken is Ownable, Token {
+    using SafeMath for uint256;
+    using SafeCast for int256;
 
     /// @dev Amount '_supply' is the total supply of tokens in existence
     /// @dev '_feedAddress' is the address of a chainlink ETH/USD price feed oracle
@@ -45,10 +50,12 @@ contract CloudToken is Ownable, Token {
         @dev When a user sends ether to the contract, we can use this function
         to determine the value of one usd in ether. Then we can peg our cloud token
         at exactly one usd or anyother value
+
      */
-    function usd_to_wei(uint256 _eth_usd, uint8 _decimals) public pure returns (uint256) {
-        uint16 power = 18 + _decimals;
-        return (10 ** power).div(_eth_usd);
+    function usdToWei(int256 _ethUsd, uint8 _decimals) public pure returns (uint256) {
+        uint256 ethUsd = _ethUsd.toUint256(); // using the SafeCast library for conversion
+        uint256 numerator = 10 ** (18 + _decimals);
+        return numerator.div(ethUsd);
     }
 
 }
