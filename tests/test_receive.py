df4712834b52c33141d99886e1f4c33b12bc3ca5
Edward Amor <edward.amor3@gmail.com>
2021-01-06 02:48:02 -0500

    fix(tests): update receive function tests

--- tests/test_receive.py
+++ tests/test_receive.py
@@ -1,12 +1,21 @@
+"""Conversion rates with Mocked Price Feed reference
+1     ETH = 10 ** 18 wei = 100 USD = 10000 XCT
+.01   ETH = 10 ** 16 wei =   1 USD =   100 XCT
+.0001 ETH = 10 ** 14 wei = .01 USD =     1 XCT
+
+XCT = CloudToken
+"""
+
+
 def test_receive_increases_balance_of_sender(beth, token, web3):
     initial_balance = token.balanceOf(beth)
-    beth.transfer(token, "1 ether")
+    beth.transfer(token, web3.toWei(0.01, "ether"))
 
-    assert token.balanceOf(beth) == initial_balance + web3.toWei(1, "ether")
+    assert token.balanceOf(beth) == initial_balance + 100
 
 
 def test_receive_increases_total_supply(beth, token, web3):
     initial_supply = token.totalSupply()
-    beth.transfer(token, "1 ether")
+    beth.transfer(token, web3.toWei(0.01, "ether"))
 
-    assert token.totalSupply() == initial_supply + web3.toWei(1, "ether")
+    assert token.totalSupply() == initial_supply + 100
