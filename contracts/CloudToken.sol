18cdf55e0d4e6e50ee21e04bde4eb9b992e85587
Edward Amor <edward.amor3@gmail.com>
2021-01-06 02:41:04 -0500

    feat: add interaction with price feed oracle

--- contracts/CloudToken.sol
+++ contracts/CloudToken.sol
@@ -5,10 +5,14 @@ pragma solidity ^0.7.5;
 // Links to use when working on https://remix.ethereum.org/#optimize=true&runs=200&evmVersion=null&version=soljson-v0.7.5+commit.eb77ed08.js
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/utils/SafeCast.sol";
+// import "https://github.com/alphachainio/chainlink-contracts/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/SafeCast.sol";
+import "@alphachain/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "./Token.sol";
 
 
@@ -18,9 +22,12 @@ contract CloudToken is Ownable, Token {
     using SafeMath for uint256;
     using SafeCast for int256;
 
+    AggregatorV3Interface private priceFeed;
+
     /// @dev Amount '_supply' is the total supply of tokens in existence
     /// @dev '_feedAddress' is the address of a chainlink ETH/USD price feed oracle
-    constructor(uint256 _supply) public {
+    constructor(uint256 _supply, address _priceFeedAddress) public {
+        priceFeed = AggregatorV3Interface(_priceFeedAddress);
         _mint(msg.sender, _supply);
     }
 
