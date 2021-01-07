fb020be0230c108848fadc28429625ef15e5803e
Edward Amor <edward.amor3@gmail.com>
2021-01-07 03:51:41 -0500

    feat: add oracle addresses dictionary for test nets

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,14 @@
 from brownie import CloudToken, PriceFeedOracle, accounts
 
+"""Chainlink Price Feed Oracles for the ETH/USD Pair
+URL: https://docs.chain.link/docs/ethereum-addresses"""
+
+ETHUSD_ORACLE_ADDRESSES = {
+    "Mainnet": "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+    "Kovan": "0x9326BFA02ADD2366b30bacB125260Af641031331",
+    "Rinkeby": "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+}
+
 
 def main():
     acct = accounts[0]
